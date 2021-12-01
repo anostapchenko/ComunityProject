e9052adceef9bafacffd2021c3d8631a9a89dfd1
infinic <admlynx@gmail.com>
2021-12-01 15:39:53 +0500

    Questions, Answers and Tags initialization added.

--- src/main/java/com/javamentor/qa/platform/service/impl/TestDataInitService.java
+++ src/main/java/com/javamentor/qa/platform/service/impl/TestDataInitService.java
@@ -1,12 +1,13 @@
 package com.javamentor.qa.platform.service.impl;
 
+import com.javamentor.qa.platform.models.entity.question.Question;
+import com.javamentor.qa.platform.models.entity.question.Tag;
+import com.javamentor.qa.platform.models.entity.question.answer.Answer;
 import com.javamentor.qa.platform.models.entity.user.Role;
 import com.javamentor.qa.platform.models.entity.user.User;
-import com.javamentor.qa.platform.service.abstracts.model.RoleService;
-import com.javamentor.qa.platform.service.abstracts.model.UserService;
+import com.javamentor.qa.platform.service.abstracts.model.*;
 import org.springframework.stereotype.Service;
 
-import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
@@ -16,13 +17,22 @@ public class TestDataInitService {
 
     private final RoleService roleService;
     private final UserService userService;
+    private final TagService tagService;
+    private final QuestionService questionService;
+    private final AnswerService answerService;
 
     private final long NUM_OF_ROLES = 3L;
     private final long NUM_OF_USERS = 10L;
+    private final long NUM_OF_TAGS = 5L;
+    private final long NUM_OF_QUESTIONS = 10L;
+    private final long NUM_OF_ANSWERS = 50L;
 
-    public TestDataInitService(RoleService roleService, UserService userService) {
+    public TestDataInitService(RoleService roleService, UserService userService, TagService tagService, QuestionService questionService, AnswerService answerService) {
         this.roleService = roleService;
         this.userService = userService;
+        this.tagService = tagService;
+        this.questionService = questionService;
+        this.answerService = answerService;
     }
 
     public void createRoles() {
@@ -57,9 +67,76 @@ public class TestDataInitService {
         userService.persistAll(users);
     }
 
+    public void createTags() {
+        List<Tag> tags = new ArrayList<>();
+        for (int i = 1; i <= NUM_OF_TAGS; i++) {
+            Tag tag = Tag.builder()
+                    .name("Tag " + i)
+                    .description("Description of tag " + i)
+                    .build();
+            tags.add(tag);
+        }
+
+        tagService.persistAll(tags);
+    }
+
+    public void createQuestions() {
+        List<Question> questions = new ArrayList<>();
+        for (int i = 1; i <= NUM_OF_QUESTIONS; i++) {
+            Question question = Question.builder()
+                    .title("Question " + i)
+                    .description("What you think about question " + i + "?")
+                    .user(getRandomUser())
+                    .tags(getRandomTagList())
+                    .build();
+            questions.add(question);
+        }
+
+        questionService.persistAll(questions);
+    }
+
+    public void createAnswers() {
+        List<Answer> answers = new ArrayList<>();
+        for (int i = 1; i <= NUM_OF_ANSWERS; i++) {
+            Answer answer = Answer.builder()
+                    .htmlBody("Answer " + i)
+                    .user(getRandomUser())
+                    .question(getRandomQuestion())
+                    .isDeleted(false)
+                    .isHelpful(false)
+                    .isDeletedByModerator(false)
+                    .build();
+            answers.add(answer);
+        }
+
+        answerService.persistAll(answers);
+    }
+
+    private List<Tag> getRandomTagList() {
+        List<Tag> tags = tagService.getAll();
+        int numOfDeleteTags = new Random().nextInt(tags.size());
+        for (int i = 0; i < numOfDeleteTags; i++) {
+            tags.remove(new Random().nextInt(tags.size()));
+        }
+        return tags;
+    }
+
+    private User getRandomUser() {
+        List<User> users = userService.getAll();
+        return users.get(new Random().nextInt(users.size()));
+    }
+
+    private Question getRandomQuestion() {
+        List<Question> questions = questionService.getAll();
+        return questions.get(new Random().nextInt(questions.size()));
+    }
+
     public void init() {
         createRoles();
         createUsers();
+        createTags();
+        createQuestions();
+        createAnswers();
     }
 
 }
