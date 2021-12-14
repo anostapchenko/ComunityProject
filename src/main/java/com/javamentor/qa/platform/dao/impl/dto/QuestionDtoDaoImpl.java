6901e5cb838271725609c9eccf483925375ae2f0
A_Ostapchenko <anostapchenko@yandex.ru>
2021-12-14 13:35:08 +0300

    AuthorReputation added

--- src/main/java/com/javamentor/qa/platform/dao/impl/dto/QuestionDtoDaoImpl.java
+++ src/main/java/com/javamentor/qa/platform/dao/impl/dto/QuestionDtoDaoImpl.java
@@ -2,32 +2,36 @@ package com.javamentor.qa.platform.dao.impl.dto;
 
 import com.javamentor.qa.platform.dao.abstracts.dto.QuestionDtoDao;
 import com.javamentor.qa.platform.models.dto.QuestionDto;
+import com.javamentor.qa.platform.models.entity.question.Question;
+import com.javamentor.qa.platform.models.entity.user.reputation.Reputation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-import javax.persistence.TypedQuery;
+import javax.persistence.*;
 import java.util.Collections;
 
 @Repository
 public class QuestionDtoDaoImpl implements QuestionDtoDao {
     @PersistenceContext
     private EntityManager entityManager;
+    private EntityTransaction entityTransaction;
 
     @Autowired
     private TagDtoDaoImpl tagDtoDaoImpl;
 
     public QuestionDto getQuestionDtoDaoById(Long id) {
 
+
         TypedQuery<QuestionDto> q = entityManager.createQuery(
                 "SELECT new com.javamentor.qa.platform.models.dto.QuestionDto(q.id, " +
                         "q.title, u.id, u.fullName, u.imageLink, q.description," +
                         "q.persistDateTime, q.lastUpdateDateTime)" +
-                        " FROM Question q JOIN q.user u  WHERE q.id =: id ", QuestionDto.class);
+                        " FROM Question q JOIN  q.user u  WHERE q.id =: id ", QuestionDto.class);
         q.setParameter("id", id);
         QuestionDto questionDto = q.getSingleResult();
-        questionDto.setAuthorReputation(666l); // не понимаю, где брать и как считать
+        Integer ID = (Integer) entityManager.createQuery("SELECT r.count FROM Reputation r").getSingleResult();
+        questionDto.setAuthorReputation(new Long(ID)); // тестовые данные в таблицу Reputation заводил в ручную
         questionDto.setViewCount(0); //(пока не считай это поле, как оно будет считаться решим позже, пусть пока будет 0)
         questionDto.setCountAnswer(333);  // не понимаю, где брать и как считать
         questionDto.setCountValuable(444);  // не понимаю, где брать и как считать
