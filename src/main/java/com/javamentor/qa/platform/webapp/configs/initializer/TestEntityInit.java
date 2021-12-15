0f6fc259875cfb58cd26d6ef89e28b0e84597af8
Yuri Kriger <yuri.kriger@gmail.com>
2021-12-15 22:26:26 +0600

    some change

--- src/main/java/com/javamentor/qa/platform/webapp/configs/initializer/TestEntityInit.java
+++ src/main/java/com/javamentor/qa/platform/webapp/configs/initializer/TestEntityInit.java
@@ -1,15 +1,11 @@
 package com.javamentor.qa.platform.webapp.configs.initializer;
 
 import com.javamentor.qa.platform.service.impl.TestDataInitService;
-import org.flywaydb.core.Flyway;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingClass;
-import org.springframework.boot.autoconfigure.flyway.FlywayMigrationStrategy;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
+
 
 @Component
 @ConditionalOnMissingClass({"org.springframework.boot.test.context.SpringBootTest"})
@@ -20,20 +16,6 @@ public class TestEntityInit implements CommandLineRunner {
     @Autowired
     public TestEntityInit(TestDataInitService testDataInitService) {
         this.testDataInitService = testDataInitService;
-    }
-
-    @Bean
-    @Profile("test")
-    public FlywayMigrationStrategy cleanMigrateStrategy() {
-        FlywayMigrationStrategy strategy = new FlywayMigrationStrategy() {
-            @Override
-            public void migrate(Flyway flyway) {
-                flyway.clean();
-                flyway.migrate();
-            }
-        };
-
-        return strategy;
     }
 
     @Override
