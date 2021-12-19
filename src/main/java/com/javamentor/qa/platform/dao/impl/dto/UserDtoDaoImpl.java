6dedf64c6fc87e8ae8d81b5a041cf1520db8a158
rashi <rashid058@mial.ru>
2021-12-19 20:42:25 +0500

    get_api_userId

--- src/main/java/com/javamentor/qa/platform/dao/impl/dto/UserDtoDaoImpl.java
+++ src/main/java/com/javamentor/qa/platform/dao/impl/dto/UserDtoDaoImpl.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
 import java.util.List;
 
 @Repository
@@ -15,13 +16,17 @@ public class UserDtoDaoImpl implements UserDtoDao {
 
     @Override
     public List<UserDto> findUserDto(Long id) {
+        List<Long> sql = entityManager.createQuery("select SUM(r.count+r.sender) from Reputation r WHERE r.author.id=:id")
+                .setParameter("id", id)
+                .getResultList();
+
         return entityManager.createQuery("SELECT NEW com.javamentor.qa.platform.models.dto.UserDto(" +
                         "rep.author.id, " +
                         "rep.author.email, " +
                         "rep.author.fullName, " +
                         "rep.author.imageLink, " +
                         "rep.author.city," +
-                        "rep.count) FROM Reputation rep where rep.author.id=:id")
+                        sql.get(0)+") FROM Reputation rep where rep.author.id=:id")
                 .setParameter("id", id)
                 .getResultList();
     }
