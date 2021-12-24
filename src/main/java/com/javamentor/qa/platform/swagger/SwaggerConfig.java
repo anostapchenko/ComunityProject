b2c6324c9bda77bb1642c6253221b5bc8aa2de02
Yuri Kriger <yuri.kriger@gmail.com>
2021-12-24 23:15:32 +0600

    Add JWT-authentication in to OpenAPI 3.0 (Swagger)

--- src/main/java/com/javamentor/qa/platform/swagger/SwaggerConfig.java
+++ src/main/java/com/javamentor/qa/platform/swagger/SwaggerConfig.java
@@ -1,8 +1,12 @@
 package com.javamentor.qa.platform.swagger;
 
+
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,7 +14,19 @@ import org.springframework.context.annotation.Configuration;
 public class SwaggerConfig {
     @Bean
     public OpenAPI customOpenAPI() {
+        final String securitySchemeName = "bearerAuth";
         return new OpenAPI()
+                .addSecurityItem(new SecurityRequirement().addList(securitySchemeName))
+                .components(
+                        new Components()
+                                .addSecuritySchemes(securitySchemeName,
+                                        new SecurityScheme()
+                                                .name(securitySchemeName)
+                                                .type(SecurityScheme.Type.HTTP)
+                                                .scheme("bearer")
+                                                .bearerFormat("JWT")
+                                )
+                )
                 .info(
                         new Info()
                                 .title("Kata Stack Overflow API")
