f2a4c11c18d70d64766362d9c4f76111115fa3ac
Sidorov Aleksey <cobetckoeyxo@mail.ru>
2021-12-26 22:28:14 +1000

    Was added description to TagDTO and TagResourceController

--- src/main/java/com/javamentor/qa/platform/models/dto/question/TagDTO.java
+++ src/main/java/com/javamentor/qa/platform/models/dto/question/TagDTO.java
@@ -1,16 +1,23 @@
 package com.javamentor.qa.platform.models.dto.question;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import java.time.LocalDateTime;
 
+@Schema(description = "TagDTO - сокращенный тег")
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class TagDTO {
 
+    @Schema(description = "Идентификатор")
     private Long id;
+
+    @Schema(description = "Имя тега")
     private String name;
+
+    @Schema(description = "Дата сохранения тега")
     private LocalDateTime persistDateTime;
 }
--- src/main/java/com/javamentor/qa/platform/webapp/controllers/rest/TagResourceController.java
+++ src/main/java/com/javamentor/qa/platform/webapp/controllers/rest/TagResourceController.java
@@ -3,6 +3,12 @@ package com.javamentor.qa.platform.webapp.controllers.rest;
 import com.javamentor.qa.platform.models.dto.question.TagDTO;
 import com.javamentor.qa.platform.models.entity.user.User;
 import com.javamentor.qa.platform.service.abstracts.dto.TagDTOService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,6 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@Tag(name = "TagResourceController", description = "Позволяет работать с тегами")
 @RestController
 public class TagResourceController {
 
@@ -21,6 +28,19 @@ public class TagResourceController {
         this.tagDTOServiceImpl = tagDTOServiceImpl;
     }
 
+    @Operation(summary = "Получение списка пользовательских тегов",
+            description = "Получение списка тегов текущего пользователя")
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Возвращает список TagDTO (id, name, persist_date)",
+                    content = {
+                            @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(implementation = List.class))
+                    }),
+            @ApiResponse(responseCode = "403", description = "Доступ запрещён")
+    })
     @GetMapping(path = "/api/user/tag/tracked")
     public @ResponseBody List<TagDTO> getTagDto(Authentication authentication) {
         User currentUser = (User)authentication.getPrincipal();
