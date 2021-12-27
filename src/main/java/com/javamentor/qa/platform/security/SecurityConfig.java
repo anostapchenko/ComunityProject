d3b5f55940711178c9c28ad4ac06acf9eafae65b
Epkkc <71571866+Epkkc@users.noreply.github.com>
2021-12-27 22:22:29 +0300

    refactoring 3

--- src/main/java/com/javamentor/qa/platform/security/SecurityConfig.java
+++ src/main/java/com/javamentor/qa/platform/security/SecurityConfig.java
@@ -35,7 +35,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter implements WebM
     @Override
     public void configure(WebSecurity web) {
         web.ignoring()
-                .antMatchers(HttpMethod.GET, "/html/**", "/js/**", "/css/**", "/images/**");
+                .antMatchers(HttpMethod.GET, "/html/**", "/js/**", "/css/**");
     }
 
     @Override
