ae442af7dbb463bece21add2d311ea0cd93ee2a0
rashi <rashid058@mial.ru>
2021-12-27 22:08:52 +0500

    get_api_question_count

--- src/main/java/com/javamentor/qa/platform/service/abstracts/model/QuestionService.java
+++ src/main/java/com/javamentor/qa/platform/service/abstracts/model/QuestionService.java
@@ -7,5 +7,4 @@ import java.util.Optional;
 public interface QuestionService extends ReadWriteService<Question, Long> {
     Optional<Long> getCountByQuestion();
 
-    Optional<Question> getQuestionByIdWithAuthor (Long id);
 }
