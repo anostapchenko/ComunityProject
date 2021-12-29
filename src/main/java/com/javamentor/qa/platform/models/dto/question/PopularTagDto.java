dfc76733c2ec691390d30bff09f7f5f5cdcd5aea
infinic <infinic66@gmail.com>
2021-12-30 00:52:05 +0500

    Popular tags

---
+++ src/main/java/com/javamentor/qa/platform/models/dto/question/PopularTagDto.java
@@ -0,0 +1,16 @@
+package com.javamentor.qa.platform.models.dto.question;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Schema(description = "Тег с количеством вопросов по нему")
+public class PopularTagDto extends TagDto {
+
+    @Schema(description = "Количество вопросов по тегу")
+    private Long countQuestion;
+}
