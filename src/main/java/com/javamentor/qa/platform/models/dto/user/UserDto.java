d377866909979b6aba6cef3f942f6777d4a3d2b6
Sidorov Aleksey <cobetckoeyxo@mail.ru>
2022-01-04 01:28:19 +1000

    Was added description

--- src/main/java/com/javamentor/qa/platform/models/dto/user/UserDto.java
+++ src/main/java/com/javamentor/qa/platform/models/dto/user/UserDto.java
@@ -1,20 +1,33 @@
 package com.javamentor.qa.platform.models.dto.user;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+@Schema(description = "UserDto - сокращенные сведения о пользователе")
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
 public class UserDto {
+    @Schema(description = "Идентификатор")
     private Long id;
+
+    @Schema(description = "email - используется в качестве username для входа в систему")
     private String email;
+
+    @Schema(description = "Имя")
     private String fullName;
+
+    @Schema(description = "Связанная картинка")
     private String linkImage;
+
+    @Schema(description = "Город")
     private String city;
+
+    @Schema(description = "Величина репутации - сумма общей репутации за вопросы и ответы")
     private int reputation;
 }
 
--- src/main/java/com/javamentor/qa/platform/webapp/controllers/rest/UserResourceController.java
+++ src/main/java/com/javamentor/qa/platform/webapp/controllers/rest/UserResourceController.java
@@ -5,6 +5,12 @@ import com.javamentor.qa.platform.models.dto.PageDTO;
 import com.javamentor.qa.platform.models.dto.user.UserDto;
 import com.javamentor.qa.platform.models.entity.pagination.PaginationData;
 import com.javamentor.qa.platform.service.abstracts.pagination.UserPageDtoService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,6 +19,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Tag(name = "User Resource Controller", description = "Получение данных о пользователях")
 public class UserResourceController {
 
     private UserPageDtoService userDtoService;
@@ -21,12 +28,29 @@ public class UserResourceController {
     public UserResourceController(UserPageDtoService userDtoService) {
         this.userDtoService = userDtoService;
     }
+
+    @Operation(summary = "Постраничное получение списка пользователей",
+            description = "Постраничное получение списка пользователей отсортированных по сумме голосов" +
+                    "за ответы и вопросы, где DownVote = -1 и UpVote = 1")
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Возвращает PageDto с вложенным массивом UserDto согласно текущей страницы" +
+                            "и количеству запрашиваемых пользователей",
+                    content = {
+                            @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(implementation = PageDTO.class))
+                    }),
+            @ApiResponse(responseCode = "403", description = "Доступ запрещён"),
+            @ApiResponse(responseCode = "400", description = "Неверная нумерация страниц")
+    })
     @GetMapping(path = "/api/user/vote")
     public ResponseEntity<PageDTO<UserDto>> getUsersByVoteAsc(@RequestParam Integer page, @RequestParam(required = false) Integer items) {
-        if(page == null || page <= 0) {
+        if (page == null || page <= 0) {
             return new ResponseEntity("Incorrect page number", HttpStatus.BAD_REQUEST);
         }
-        if(items == null || items <= 0) {
+        if (items == null || items <= 0) {
             items = 10;
         }
         PaginationData data = new PaginationData(page, items,
