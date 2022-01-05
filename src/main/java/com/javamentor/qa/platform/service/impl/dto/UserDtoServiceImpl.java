c98ca28a0a307402741cb9e85fe27eb9f9f64487
rashi <rashid058@mial.ru>
2022-01-05 20:58:31 +0500

    userdtoservice_ok

--- src/main/java/com/javamentor/qa/platform/service/impl/dto/UserDtoServiceImpl.java
+++ src/main/java/com/javamentor/qa/platform/service/impl/dto/UserDtoServiceImpl.java
@@ -1,19 +1,24 @@
 package com.javamentor.qa.platform.service.impl.dto;
 
 import com.javamentor.qa.platform.dao.abstracts.dto.UserDtoDao;
+import com.javamentor.qa.platform.dao.abstracts.pagination.PageDtoDao;
 import com.javamentor.qa.platform.models.dto.UserDto;
 import com.javamentor.qa.platform.service.abstracts.dto.UserDtoService;
+import com.javamentor.qa.platform.service.abstracts.pagination.PageDtoService;
+import com.javamentor.qa.platform.service.impl.pagination.DtoServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
-import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 @Service
-public class UserDtoServiceImpl implements UserDtoService {
+public class UserDtoServiceImpl extends DtoServiceImpl<UserDto> implements UserDtoService, PageDtoService<UserDto> {
     private final UserDtoDao userDtoDao;
 
-    public UserDtoServiceImpl(UserDtoDao userDtoDao) {
+    @Autowired
+    public UserDtoServiceImpl(Map<String, PageDtoDao<UserDto>> daoMap, UserDtoDao userDtoDao) {
+        super(daoMap);
         this.userDtoDao = userDtoDao;
     }
 
@@ -21,4 +26,5 @@ public class UserDtoServiceImpl implements UserDtoService {
     public Optional<UserDto> findUserDtoById(Long id) {
         return userDtoDao.findUserDto(id);
     }
+
 }
