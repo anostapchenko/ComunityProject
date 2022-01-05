50454bef79658be3aa4b1ebaffa6b025d672c5e4
rashi <rashid058@mial.ru>
2022-01-05 19:15:34 +0500

    git_conflict

--- src/main/java/com/javamentor/qa/platform/models/dto/UserDto.java
+++ src/main/java/com/javamentor/qa/platform/models/dto/UserDto.java
@@ -1,33 +1,5 @@
 package com.javamentor.qa.platform.models.dto;
 
-<<<<<<< HEAD
-import io.swagger.v3.oas.annotations.media.Schema;
-import lombok.Data;
-
-@Data
-public class UserDto {
-    @Schema(description = "Идентификатор", accessMode = Schema.AccessMode.READ_ONLY)
-    private Long id;
-    @Schema(description = "email", example = "test@mail.ru")
-    private String email;
-    @Schema(description = "Name", example = "User")
-    private String fullName;
-    @Schema(description = "linkImage", example = "linkImage")
-    private String linkImage;
-    @Schema(description = "city", example = "Moskow")
-    private String city;
-    @Schema(description = "Репутация", accessMode = Schema.AccessMode.READ_ONLY)
-    private Long reputation;
-
-    public UserDto(Long id, String email, String fullName, String linkImage, String city, Long reputation) {
-        this.id = id;
-        this.email = email;
-        this.fullName = fullName;
-        this.linkImage = linkImage;
-        this.city = city;
-        this.reputation = reputation;
-    }
-=======
 import lombok.*;
 
 @Getter
@@ -42,7 +14,5 @@ public class UserDto {
     private String imageLink;
     private String city;
     private Long reputation;
-
->>>>>>> dev
 
 }
