0a57dfe3dd05a916d3a9efe649bc6a818391b9b7
Yuri Kriger <yuri.kriger@gmail.com>
2022-01-10 06:30:05 +0600

    QuestionServiceImpl

--- src/main/java/com/javamentor/qa/platform/service/impl/model/QuestionServiceImpl.java
+++ src/main/java/com/javamentor/qa/platform/service/impl/model/QuestionServiceImpl.java
@@ -5,7 +5,6 @@ import com.javamentor.qa.platform.dao.abstracts.model.TagDao;
 import com.javamentor.qa.platform.models.entity.question.Question;
 import com.javamentor.qa.platform.models.entity.question.Tag;
 import com.javamentor.qa.platform.service.abstracts.model.QuestionService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -20,7 +19,6 @@ public class QuestionServiceImpl extends ReadWriteServiceImpl<Question, Long> im
     private final QuestionDao questionDao;
     private final TagDao tagDao;
 
-    @Autowired
     public QuestionServiceImpl(QuestionDao questionDao, TagDao tagDao) {
         super(questionDao);
         this.questionDao = questionDao;
