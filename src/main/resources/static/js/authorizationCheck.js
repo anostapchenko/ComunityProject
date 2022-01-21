862e6266a5511b01de1dce0071585b51427094c6
IanOleg <ianoleg86@gmail.com>
2022-01-21 19:28:31 +0300

    authorizationCheck.js Переделываю скрипт js

--- src/main/resources/static/js/authorizationCheck.js
+++ src/main/resources/static/js/authorizationCheck.js
@@ -1,7 +1,9 @@
 
-fetch("http://localhost:8091/api/auth/check")
-    .then(response => {
-        if (!response.ok){
-            window.location.assign("http://localhost:8091/login");
-        }
-    });
+var request = new XMLHttpRequest();
+request.open('GET', 'http://localhost:8091/api/auth/check', false);
+request.send(null);
+
+if (request.status !== 200) {
+    window.location.assign("http://localhost:8091/login");
+}
+
