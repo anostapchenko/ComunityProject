4ab556aeb079040b2feeba725debec9b6ef71695
Globium <lol336@yandex.ru>
2022-01-23 03:15:21 +0300

    Working on task

--- src/main/java/com/javamentor/qa/platform/webapp/controllers/rest/QuestionResourceController.java
+++ src/main/java/com/javamentor/qa/platform/webapp/controllers/rest/QuestionResourceController.java
@@ -1,6 +1,11 @@
 package com.javamentor.qa.platform.webapp.controllers.rest;
 
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.javamentor.qa.platform.dao.impl.pagination.QuestionPageWithoutAnswerDtoDao;
 import com.javamentor.qa.platform.exception.ConstrainException;
 import com.javamentor.qa.platform.models.dto.PageDTO;
@@ -27,6 +32,8 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDateTime;
+import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
@@ -74,14 +81,14 @@ public class QuestionResourceController {
     )
     public ResponseEntity<?> upVote(@PathVariable("questionId") Long questionId) {
         Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-        User user =(User) auth.getPrincipal();
+        User user = (User) auth.getPrincipal();
         Long userId = user.getId();
         Question question = questionService
                 .getQuestionByIdWithAuthor(questionId)
                 .orElseThrow(() -> new ConstrainException("Can't find question with id:" + questionId));
         int countUpVote = 10;
         if (voteQuestionService.validateUserVoteByQuestionIdAndUserId(questionId, userId)) {
-            VoteQuestion voteQuestion = new VoteQuestion(user,question,VoteType.UP_VOTE,countUpVote);
+            VoteQuestion voteQuestion = new VoteQuestion(user, question, VoteType.UP_VOTE, countUpVote);
             voteQuestionService.persist(voteQuestion);
             return new ResponseEntity<>(voteQuestionService.getVoteByQuestionId(questionId), HttpStatus.OK);
         }
@@ -95,14 +102,14 @@ public class QuestionResourceController {
     )
     public ResponseEntity<?> downVote(@PathVariable("questionId") Long questionId) {
         Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-        User user =(User) auth.getPrincipal();
+        User user = (User) auth.getPrincipal();
         Long userId = user.getId();
         Question question = questionService
                 .getQuestionByIdWithAuthor(questionId)
                 .orElseThrow(() -> new ConstrainException("Can't find question with id:" + questionId));
         int countDownVote = -5;
         if (voteQuestionService.validateUserVoteByQuestionIdAndUserId(questionId, userId)) {
-            VoteQuestion voteQuestion = new VoteQuestion(user,question,VoteType.DOWN_VOTE,countDownVote);
+            VoteQuestion voteQuestion = new VoteQuestion(user, question, VoteType.DOWN_VOTE, countDownVote);
             voteQuestionService.persist(voteQuestion);
             return new ResponseEntity<>(voteQuestionService.getVoteByQuestionId(questionId), HttpStatus.OK);
         }
@@ -115,9 +122,9 @@ public class QuestionResourceController {
             @Content(mediaType = "application/json")
     })
 
-    public ResponseEntity<?> getQuestion(@PathVariable Long id){
+    public ResponseEntity<?> getQuestion(@PathVariable Long id) {
         Optional<QuestionDto> q = questionDtoService.getQuestionDtoServiceById(id);
-        if(q.isPresent()){
+        if (q.isPresent()) {
             return new ResponseEntity<>(q.get(), HttpStatus.OK);
         }
         return new ResponseEntity<>("Question number not exist!", HttpStatus.BAD_REQUEST);
@@ -138,15 +145,30 @@ public class QuestionResourceController {
                                     mediaType = "application/json")
                     }),
     })
-    public ResponseEntity<PageDTO<QuestionDto>> getQuestionsWithNoAnswer(@RequestParam int page, @RequestParam(required = false, defaultValue = "10") int items,
-                                                                       @RequestParam(required = false) String trackedTags,
-                                                                       @RequestParam (required = false) String ignoredTags) {
+    public ResponseEntity<PageDTO<QuestionDto>> getQuestionsWithNoAnswer(@RequestParam int page,
+                                                                         @RequestParam(required = false, defaultValue = "10") int items,
+                                                                         @RequestParam(required = false) String trackedTags,
+                                                                         @RequestParam(required = false) String ignoredTags) {
+
         PaginationData data = new PaginationData(page, items, QuestionPageWithoutAnswerDtoDao.class.getSimpleName());
 
         PageDTO<QuestionDto> pageDTO = questionDtoService.getPageDto(data);
-        List<QuestionDto> questionDtos = pageDTO.getItems();
+        List<QuestionDto> questionDtoList = pageDTO.getItems();
 
-        questionDtos.removeIf(dto -> !dto.getListTagDto().contains(trackedTags) && dto.getListTagDto().contains(ignoredTags));
+        try {
+            ObjectMapper objectMapper = new ObjectMapper().registerModule(new JavaTimeModule());
+
+            if(trackedTags != null) {
+                List<TagDto> trackedTagList = objectMapper.readValue(trackedTags, new TypeReference<>() {});
+                questionDtoList.removeIf(dto -> !dto.getListTagDto().containsAll(trackedTagList));
+            }
+            if(ignoredTags != null) {
+                List<TagDto> ignoredTagList = objectMapper.readValue(ignoredTags, new TypeReference<>() {});
+                questionDtoList.removeIf(dto -> !Collections.disjoint(dto.getListTagDto(), ignoredTagList));
+            }
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
 
         return new ResponseEntity<>(pageDTO, HttpStatus.OK);
     }
