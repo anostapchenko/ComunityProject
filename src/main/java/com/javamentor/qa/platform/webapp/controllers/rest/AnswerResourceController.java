2519eef3234b02ce0ced4d805d398cfedfc3a999
Konstantin <lord-konst@mail.ru>
2022-03-01 17:15:37 +0300

    Первый коммит

--- src/main/java/com/javamentor/qa/platform/webapp/controllers/rest/AnswerResourceController.java
+++ src/main/java/com/javamentor/qa/platform/webapp/controllers/rest/AnswerResourceController.java
@@ -20,11 +20,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.List;
@@ -32,6 +28,7 @@ import java.util.Optional;
 
 @Tag(name = "AnswerResourceController", description = "Позволяет работать с ответами на вопросы")
 @RestController
+@RequestMapping("api/user/question/{questionId}/answer")
 public class AnswerResourceController {
 
     private VoteAnswerService voteAnswerService;
@@ -66,7 +63,7 @@ public class AnswerResourceController {
             @ApiResponse(responseCode = "403", description = "Доступ запрещён"),
             @ApiResponse(responseCode = "404", description = "Not Found, если нет id нужного ответа")
     })
-    @PostMapping(path = "api/user/question/{questionId}/answer/{id}/upVote")
+    @PostMapping(path = "/{id}/upVote")
     public ResponseEntity<?> upVote(@PathVariable(name = "id") long answerId,
                                        Authentication authentication) {
         User currentUser = (User) authentication.getPrincipal();
@@ -90,7 +87,7 @@ public class AnswerResourceController {
             @ApiResponse(responseCode = "403", description = "Доступ запрещён"),
             @ApiResponse(responseCode = "404", description = "Not Found, если нет id нужного ответа")
     })
-    @PostMapping(path = "api/user/question/{questionId}/answer/{id}/downVote")
+    @PostMapping(path = "/{id}/downVote")
     public ResponseEntity<?> downVote(
             @PathVariable(name = "id") long answerId,
             Authentication authentication) {
@@ -125,7 +122,7 @@ public class AnswerResourceController {
     @ApiResponse(responseCode = "400", description = "Ответ не добавлен", content = {
             @Content(mediaType = "application/json")
     })
-    @PostMapping("api/user/question/{questionId}/answer/add")
+    @PostMapping("/add")
     public ResponseEntity<?> createAnswer(@PathVariable Long questionId,
                                           @Valid @RequestBody String bodyAnswer,
                                           Authentication authentication) {
@@ -156,9 +153,15 @@ public class AnswerResourceController {
                                     mediaType = "application/json")
                     }),
     })
-    @GetMapping("api/user/question/{questionId}/answer")
+    @GetMapping
     public ResponseEntity<List<AnswerDTO>> getAnswers(@PathVariable Long questionId) {
         return new ResponseEntity<>(answerDtoService.getAllAnswerDtoByQuestionId(questionId), HttpStatus.OK);
+    }
+
+    @DeleteMapping(path = "/{id}/delete")
+    public ResponseEntity<?> deleteAnswwer(@PathVariable(name = "id") long answerId,
+                                      Authentication authentication) {
+
     }
 }
 
