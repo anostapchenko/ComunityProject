9b70286dd7fe4dd0f82f0a332d92971ff73337f1
Konstantin <lord-konst@mail.ru>
2022-03-11 20:38:45 +0300

    Промежуточный коммит

--- src/main/resources/static/js/users.js
+++ src/main/resources/static/js/users.js
@@ -36,3 +36,47 @@ function getAllUsers() {
 
 
 // getAllUsers()
+
+
+
+//создаем новый объект пагинации и передаем аргументы в конструктор
+let pagination = new Pagination(
+    'http://localhost:8091/api/user/reputation',            //url
+    4,                                                //количество объектов
+    'users_grid',                             //id div куда будут вставляться объекты
+    'users_navigation',                        //id div куду будет вставляться нумерация
+
+    function (arrayObjects) {                         //функция, которая задаёт - как будут вставляться объекты
+        let ul = document.createElement('div');        //здесь был создан корневой узел(список)
+        ul.className = "card-deck";
+        if (arrayObjects != null && arrayObjects.length > 0) { //проверка массива с объектами
+            for (let num = 0; num < arrayObjects.length; num++) {
+                let li = document.createElement('div'); //для каждого объекта создаем узел
+                li.className = "card border-0";
+                li.insertAdjacentHTML('beforeend',
+                    `
+            <div class="user-card">
+                <div><img src=${arrayObjects[num].imageLink} class="avatar card-img-top" alt="..."></div>
+                <div class="card-body">
+                    <div class="card-title"><a href="#">${arrayObjects[num].fullName}</a></div>
+                    <div class="card-text small">${arrayObjects[num].city}</div>
+                    <div class="card-text font-weight-bolder small">${arrayObjects[num].reputation}</div>
+                    <div class="card-text small"><a href="#">python</a>, <a href="#">java</a>, <a href="#">c#</a></div>
+                </div>
+            </div>
+        `); //помещаем текстовое представление в узел
+                ul.appendChild(li);             //добавляем узел в корневой
+            }
+        }
+        return ul;
+    });
+
+init();
+
+function showPage(event, num) {
+    pagination.showPage(event, num);
+}
+
+async function init() {
+    await pagination.showPage(null, 1);
+}
