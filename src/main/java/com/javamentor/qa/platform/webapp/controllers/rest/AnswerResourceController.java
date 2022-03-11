c42faa92154c30df8a3244099f6d84d3d076eeff
Konstantin <lord-konst@mail.ru>
2022-03-11 08:30:42 +0300

    Вернул старый updateAnswer

--- src/main/java/com/javamentor/qa/platform/webapp/controllers/rest/AnswerResourceController.java
+++ src/main/java/com/javamentor/qa/platform/webapp/controllers/rest/AnswerResourceController.java
@@ -205,14 +205,31 @@ public class AnswerResourceController {
     @PutMapping(path = "/{id}/update")
     public ResponseEntity<?> updateAnswer(@PathVariable(name = "id") long answerId,
                                           @Valid @RequestBody String htmlBody) {
-        Optional<AnswerDTO> answerDtoOpt = answerDtoService.getAnswerDtoById(answerId);
-        if (answerDtoOpt.isEmpty()) {
+        Optional<Answer> answerOpt = answerService.getById(answerId);
+        if (answerOpt.isEmpty()) {
             return new ResponseEntity<>("Can't find answer with id:" + answerId, HttpStatus.BAD_REQUEST);
         }
-        AnswerDTO answerDTO = answerDtoOpt.get();
-        answerDTO.setHtmlBody(htmlBody);
-        answerService.update(answerConverter.answerDTOToAnswer(answerDTO));
-        return new ResponseEntity<>(answerDTO, HttpStatus.OK);
+        Answer answer = answerOpt.get();
+        answer.setHtmlBody(htmlBody);
+        answerService.update(answer);
+
+        return new ResponseEntity<>(answerDtoService.getAnswerDtoById(answerId), HttpStatus.OK);
     }
+
+
+
+
+
+//    public ResponseEntity<?> updateAnswer(@PathVariable(name = "id") long answerId,
+//                                          @Valid @RequestBody String htmlBody) {
+//        Optional<AnswerDTO> answerDtoOpt = answerDtoService.getAnswerDtoById(answerId);
+//        if (answerDtoOpt.isEmpty()) {
+//            return new ResponseEntity<>("Can't find answer with id:" + answerId, HttpStatus.BAD_REQUEST);
+//        }
+//        AnswerDTO answerDTO = answerDtoOpt.get();
+//        answerDTO.setHtmlBody(htmlBody);
+//        answerService.update(answerConverter.answerDTOToAnswer(answerDTO));
+//        return new ResponseEntity<>(answerDTO, HttpStatus.OK);
+//    }
 }
 
