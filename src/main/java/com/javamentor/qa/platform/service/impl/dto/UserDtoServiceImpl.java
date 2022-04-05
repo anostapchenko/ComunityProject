5b8b9e609d376579dee8e94a0f9a8c8d54e49647
Amperchik <aleksejjrdnv@rambler.ru>
2022-04-05 21:10:42 +0300

    Исправлен GIT конфликт Реализовано api в UserResourceController: /api/user/profile/questions возравщающее список всех сущностей UserProfileQuestionDto на основе вопросов пользователя

--- src/main/java/com/javamentor/qa/platform/service/impl/dto/UserDtoServiceImpl.java
+++ src/main/java/com/javamentor/qa/platform/service/impl/dto/UserDtoServiceImpl.java
@@ -3,7 +3,6 @@ package com.javamentor.qa.platform.service.impl.dto;
 import com.javamentor.qa.platform.dao.abstracts.dto.TagDtoDao;
 import com.javamentor.qa.platform.dao.abstracts.dto.UserDtoDao;
 import com.javamentor.qa.platform.dao.abstracts.pagination.PageDtoDao;
-import com.javamentor.qa.platform.models.dto.QuestionViewDto;
 import com.javamentor.qa.platform.models.dto.UserDto;
 import com.javamentor.qa.platform.models.dto.UserProfileQuestionDto;
 import com.javamentor.qa.platform.service.abstracts.dto.UserDtoService;
